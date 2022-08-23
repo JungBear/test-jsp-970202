ca90077d8a604fa04c8b3e4c3e3d74606c3e4d23
JungBear <mod459@naver.com>
2022-08-23 15:20:44 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -57,4 +57,81 @@ def recommen2d():
     location1 = "\'%%" + location + "%%\'"
     df=start.db_select(Benefits1,job1,age,location1,yes_no1)
     print(df)
+    name=df['name']
+    URL=df['url']
+    if len(df) > 0:
+        responseBody = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "listCard": {
+                            "header": {
+                                "title": "공고 내역입니다."
+                            },
+                            "items": [
+                                {
+                                    "title": name[0],
+                                    "imageUrl": "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+                                    "link": {
+                                        "web": URL[0]
+                                    }
+                                },
+                                {
+                                    "title": name[1],
+                                    "imageUrl": "http://k.kakaocdn.net/dn/N4Epz/btqqHCfF5II/a3kMRckYml1NLPEo7nqTmK/1x1.jpg",
+                                    "link": {
+                                        "web": URL[1]
+                                    }
+                                },
+                                {
+                                    "title": name[2],
+                                    "imageUrl": "http://k.kakaocdn.net/dn/bE8AKO/btqqFHI6vDQ/mWZGNbLIOlTv3oVF1gzXKK/1x1.jpg",
+                                    "link": {
+                                        "web": URL[2]
+                                    }
+                                }
+                            ],
+                            "buttons": [
+                                {
+                                    "label": "더보기",
+                                    "action": "webLink",
+                                    "webLinkUrl": "https://www.applyhome.co.kr/ai/aia/selectAPTLttotPblancListView.do#"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    else :
+        responseBody = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "listCard": {
+                            "header": {
+                                "title": "공고 내역입니다."
+                            },
+                            "items": [
+                                {
+                                    "title": '현재 모집중인 공고가 없습니다.',
+                                },
+                            ],
+                            "buttons": [
+                                {
+                                    "label": "다른공고더보기",
+                                    "action": "webLink",
+                                    "webLinkUrl": "https://www.applyhome.co.kr/ai/aia/selectAPTLttotPblancListView.do#"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
 
+
+    
+    return responseBody
